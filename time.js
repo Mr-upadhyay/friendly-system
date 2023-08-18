6645976b82decd53406b8e9291136af7ed32725e
Mr-upadhyay <63930261+Mr-upadhyay@users.noreply.github.com>
2023-08-19 00:27:35 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,5 +1,2 @@
-var place = document.querySelector("#place").value;
-var currentTime = new Date();
-var timeZoneOffset = currentTime.getTimezoneOffset();
-var timeInPlace = new Date(currentTime.getTime() + (timeZoneOffset * 60 * 60 * 1000));
-document.querySelector("#time").innerHTML = timeInPlace.toLocaleTimeString();
+var color = document.querySelector("#color").value;
+document.querySelector("#color").innerHTML = color;
