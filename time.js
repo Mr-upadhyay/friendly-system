65f871ccbdacd2fff9338737e9fddf95623c2f7b
Mr-upadhyay <63930261+Mr-upadhyay@users.noreply.github.com>
2023-08-19 07:38:03 +0530

    Update time.js

--- time.js
+++ time.js
@@ -1,2 +1,31 @@
-var color = document.querySelector("#color").value;
-document.querySelector("#color").innerHTML = color;
+const todoList = [];
+
+const addTask = () => {
+  const taskName = document.getElementById("task-name").value;
+  todoList.push({
+    name: taskName,
+    completed: false
+  });
+
+  updateTodoList();
+};
+
+const updateTodoList = () => {
+  const todoListElement = document.getElementById("todo-list");
+  todoListElement.innerHTML = "";
+
+  for (const task of todoList) {
+    const taskElement = document.createElement("li");
+    taskElement.textContent = task.name;
+
+    if (task.completed) {
+      taskElement.classList.add("completed");
+    }
+
+    todoListElement.appendChild(taskElement);
+  }
+};
+
+document.getElementById("add-task-button").addEventListener("click", addTask);
+
+updateTodoList();
